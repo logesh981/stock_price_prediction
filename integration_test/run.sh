293a5dd989e1c5a3e45a94a69cb0925d3dfd9ed3
Logesh_98 <logesh.venkat@gmail.com>
2025-07-21 21:03:45 +0100

    adding integration tests

---
+++ integration_test/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Navigate to the directory containing the script
+cd "$(dirname "$0")"
+
+# Tag Docker image with timestamp
+LOCAL_TAG=$(date +"%Y-%m-%d-%H-%M")
+export LOCAL_IMAGE_NAME="stock_api:${LOCAL_TAG}"
+
+
+echo "[INFO] Starting Docker Compose services"
+docker-compose -f ../docker-compose.api.yaml up -d
+
+echo "[INFO] Running integration tests"
+if pipenv run python test_docker.py; then
+    echo "[INFO] Tests passed. Shutting down containers."
+    docker-compose down
+else
+    echo "[ERROR] Tests failed. Showing logs..."
+    docker-compose -f ../docker-compose.api.yaml logs
+    docker-compose -f ../docker-compose.api.yaml down
+    exit 1
+fi
